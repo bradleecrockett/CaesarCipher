66985514d907d9a11917d9785f628919485cfa41
Brad Crockett <bradleecrockett@gmail.com>
2020-09-22 14:29:18 -0700

    Added shell program caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,44 @@
+# Name:
+# Date:
+# Period:
+# Lab: Casear Cipher
+# Description: Implement a program to encode and decode messages with a simple Caesar cipher.
+
+
+# TODO 1) Define the encoder function
+def encode(msg, key):
+
+
+    return 0
+
+
+
+
+# TODO 2) Define the decoder function
+def decode(msg, key):
+
+
+    return 0
+
+
+
+# TODO 3) Define the main() function
+def main():
+    # TODO 3a) Prompt the user for encode or decode
+
+    # TODO 3b) Prompt the user for the message
+
+
+    # TODO 3c) Prompt the user for the key
+
+
+    # TODO 3d) Use the appropriate function to encode or decode the message
+
+    # TODO 3e) Print the encoded or decoded result
+    print()
+
+
+
+if __name__ == '__main__':
+    main()
+
